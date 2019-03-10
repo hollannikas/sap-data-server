783421dbf702013ac61079e8a7b6b1d5766834cb
Rudolf Poels <rudolf.poels@futurice.com>
2019-03-10 12:55:25 +0200

    use data.zip in stead

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.zip "0.1.3"]]
   :main ^:skip-aot tylsimys.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/tylsimys/core.clj
+++ src/tylsimys/core.clj
@@ -1,23 +1,20 @@
 (ns tylsimys.core
+  (:use [clojure.data.zip.xml])
   (:require [clojure.xml :as xml]
             [clojure.zip :as zip]))
 
-(defn zip-map
-  [f node]
-  (loop [node node]
-    (if (identical? (zip/next node) node)
-      (zip/root node)
-      (if (zip/branch? node)
-        (recur (zip/next node))
-        (recur (-> node 
-                   (zip/edit f)
-                   zip/next))))))
-  
-(defn get-rules
-  [root-loc]
-  (zip-map println root-loc))
+(defn get-parts
+  [header]
+  (xml-> header
+         :CONDP))
 
-(defn get-root-loc
+(defn get-headers
+  [root]
+  (xml-> root
+         :IDOC
+         :CONDH))
+
+(defn parse-xml
   "Read xml from file name"
   [filename]
   (zip/xml-zip
@@ -26,8 +23,13 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-    (get-rules
-      (get-root-loc "test/tylsimys/TEST_COND_A.xml")))
+  (println
+    (get-headers
+      (parse-xml "test/tylsimys/TEST_COND_A.xml"))))
 
 (comment
-         (zip/children (get-root-loc "test/tylsimys/TEST_COND_A.xml")))
+  (map zip/node
+    (get-headers
+      (parse-xml "test/tylsimys/TEST_COND_A.xml")))
+  (xml-> (parse-xml "test/tylsimys/TEST_COND_A.xml")
+         :CONDH))
